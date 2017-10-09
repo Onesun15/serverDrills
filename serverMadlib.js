2be91eb2e40c4a9399fd3fc0dc73827afb9ed273
Onesun15 <sunnysharma15@hotmail.com>
2017-10-09 16:21:51 -0400

    got Madlib working

--- serverMadlib.js
+++ serverMadlib.js
@@ -6,14 +6,23 @@ const PORT = 8080;
 
 app.use(express.static(__dirname + '/'));
 
-app.get('/echo/:what', (req, res) => {
-  
-  const {hostname, query, params} = req;
-  res.json({
-    hostname,
-    query,
-    params
-  });
+function madLib(adjective1, place, adjective2, adverb, adjective3, name, noun) {
+  const madLibstring = `There's a ${adjective1} new ${name} in ${place} and everyone's talking. Stunningly ${adjective2} and ${adverb} ${adjective3}, all the cool kids know it. However, ${name} has a secret - ${name}'s a vile vampire.
+Will it end with a bite, or with a stake through the ${noun}?`;
+  return madLibstring;
+}
+
+// app.get('/echo/', (req, res) => {
+//   res.send('hello');
+// });
+
+app.get('/echo/', (req, res) => {
+  //console.log(req.query);
+  const {adjective1, place, adjective2, adverb, adjective3, name, noun} = req.query;
+  //console.log(req.query);
+//   res.send(`There's a ${adjective1} new ${name} in ${place} and everyone's talking. Stunningly ${adjective2} and ${adverb} ${adjective3}, all the cool kids know it. However, ${name} has a secret - ${name}'s a vile vampire.
+//   Will it end with a bite, or with a stake through the ${noun}?`);
+  res.send(madLib(adjective1, place, adjective2, adverb, adjective3, name, noun));
 });
 
 app.listen(PORT, () => {
