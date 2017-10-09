e487621546d022e59ae202ba65defa15ac0a7302
Onesun15 <sunnysharma15@hotmail.com>
2017-10-09 16:27:13 -0400

    refactored

--- serverMadlib.js
+++ serverMadlib.js
@@ -12,16 +12,8 @@ Will it end with a bite, or with a stake through the ${noun}?`;
   return madLibstring;
 }
 
-// app.get('/echo/', (req, res) => {
-//   res.send('hello');
-// });
-
 app.get('/echo/', (req, res) => {
-  //console.log(req.query);
   const {adjective1, place, adjective2, adverb, adjective3, name, noun} = req.query;
-  //console.log(req.query);
-//   res.send(`There's a ${adjective1} new ${name} in ${place} and everyone's talking. Stunningly ${adjective2} and ${adverb} ${adjective3}, all the cool kids know it. However, ${name} has a secret - ${name}'s a vile vampire.
-//   Will it end with a bite, or with a stake through the ${noun}?`);
   res.send(madLib(adjective1, place, adjective2, adverb, adjective3, name, noun));
 });
 
